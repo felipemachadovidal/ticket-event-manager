e500aa1a72c67dee28dd854930e5e4dc02c07328
Felipe <lipem61@gmail.com>
2024-12-24 15:42:20 -0300

    feat TicketRepository add

--- ms-ticket-manager/src/main/java/com/compassuol/ms_ticket_manager/model/Ticket.java
+++ ms-ticket-manager/src/main/java/com/compassuol/ms_ticket_manager/model/Ticket.java
@@ -2,15 +2,36 @@ package com.compassuol.ms_ticket_manager.model;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
-@Getter @Setter
-public class Ticket implements Serializable {
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+@Builder @Getter @Setter
+@Document(collection = "tickets")
+public class Ticket implements Serializable{
     private static final long serialVersionUID = 1L;
 
     @Id
-    int ticketnumber;
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String status; // e.g., OPEN, IN_PROGRESS, CLOSED
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+}
 
 
 
--- ms-ticket-manager/src/main/java/com/compassuol/ms_ticket_manager/repository/TicketRepository.java
+++ ms-ticket-manager/src/main/java/com/compassuol/ms_ticket_manager/repository/TicketRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TicketRepository extends MongoRepository<Ticket,String > {
+public interface TicketRepository extends MongoRepository<Ticket, Long> {
 }
