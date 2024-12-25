7aaf11af9ccede0b2c312ff224e3e4bc6d798719
Felipe <lipem61@gmail.com>
2024-12-25 10:54:27 -0300

    Open config add

---
+++ ms-event-manager/src/main/java/com/compassuol/ms_event_manager/client/ViaCepClient.java
@@ -0,0 +1,12 @@
+package com.compassuol.ms_event_manager.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepClient {
+
+    @GetMapping("/{cep}/json")
+    ViaCepResponse getAddressByCep(@PathVariable("cep") String cep);
+}
