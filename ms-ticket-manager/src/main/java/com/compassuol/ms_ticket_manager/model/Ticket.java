24ff2966dbb8ec5183b0e0664394ada20af15f69
Felipe <lipem61@gmail.com>
2024-12-27 11:15:17 -0300

    feat Modelmapper and TickeService class add

--- ms-ticket-manager/src/main/java/com/compassuol/ms_ticket_manager/model/Ticket.java
+++ ms-ticket-manager/src/main/java/com/compassuol/ms_ticket_manager/model/Ticket.java
@@ -2,7 +2,6 @@ package com.compassuol.ms_ticket_manager.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -39,6 +38,7 @@ public class Ticket implements Serializable{
     private String status;
 
     private Double brlAmount;
+
     private Double usdAmount;
 
 
---
+++ ms-ticket-manager/src/main/java/com/compassuol/ms_ticket_manager/service/TicketService.java
@@ -0,0 +1,8 @@
+package com.compassuol.ms_ticket_manager.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+}
